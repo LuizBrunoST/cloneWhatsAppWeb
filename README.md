1b8561e4b87d2688c18fbd95f89588626c33e486
LUMATECH apps e games <64446877+LuizBrunoST@users.noreply.github.com>
2023-04-18 21:29:13 -0300

    Alguns bugs Corrigidos
    add editor de perfil, title, sistemas de abas para conversas.

--- README.md
+++ README.md
@@ -8,3 +8,76 @@ Screenshot
 - Jquery 3.6.0
 - w3.css 4.1
 - Font Awesome 6.4.0
+
+
+```html
+        <main id="boxMsgContato" class="w3-display-container w3-animate-top" style="z-index:3;margin-left:400px;height:768px;">
+            <div class="w3-bar w3-top w3-blue" style="width:966!important;">
+                <div class="w3-left">
+                    <span class="w3-bar-item">
+                        <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="user" style="width:40px;height:40px;">
+                        <span class="w3-large w3-margin-left w3-font-bold">Contato 0</span>
+                    </span>
+                </div>
+    
+                <div class="w3-right w3-padding" style="z-index:-4;">
+                    <span class="w3-bar-item w3-button w3-round"><i class="fa-solid fa-magnifying-glass"></i></span>
+                    <span class="w3-bar-item w3-button w3-round"><i class="fa-solid fa-ellipsis-vertical"></i></span>
+                </div>
+            </div>
+    
+            <div class="w3-container" style="margin-top:60px;">
+                <div>
+                    <div class="w3-card w3-round w3-border w3-white w3-padding w3-margin-top w3-bar">
+                        <span class="w3-left">
+                            <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                        </span>
+                        <span class="w3-left w3-bar-item">Olá bom dia tudo bem com vc?</span>
+                    </div>
+    
+    
+                    <div class="w3-card w3-round w3-border w3-light-gray w3-padding w3-margin-top w3-bar">
+                        <span class="w3-right">
+                            <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                        </span>
+                        <span class="w3-right w3-bar-item">Tudo sim, e com você?</span>
+                    </div>
+    
+    
+                    <div class="w3-card w3-round w3-border w3-white w3-padding w3-margin-top w3-bar" style="width:auto;">
+                        <span class="w3-left">
+                            <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                        </span>
+                        <span class="w3-left w3-bar-item">Eu vou bem tambem</span>
+                    </div>
+    
+    
+                    <div class="w3-card w3-round w3-border w3-light-gray w3-padding w3-margin-top w3-bar">
+                        <span class="w3-right">
+                            <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                        </span>
+                        <span class="w3-right w3-bar-item">Tudo certo :)</span>
+                    </div>
+    
+    
+                </div>
+            </div>
+    
+            <div class="w3-display-bottomright w3-bar">
+                <div class="w3-row">
+                    <div class="w3-col w3-left" style="width:93px;">
+                        <div class="w3-bar">
+                            <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-face-smile"></i></span>
+                            <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-paperclip"></i></span>
+                        </div>
+                    </div>
+                    <div class="w3-col w3-right" style="width:93px;">
+                        <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-microphone"></i></span>
+                        <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-paper-plane"></i></span>
+                    </div>
+                    <div class="w3-rest">
+                        <input class="w3-input" type="text" placeholder="Mensagem">
+                    </div>
+                </div>
+            </div>
+        </main>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function clickMenu(elemento) {
     var x = document.getElementById(elemento);
-    if (x.className.indexOf("w3.show") == -1) {
+    if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
         x.className = x.className.replace(" w3-show", "");
@@ -13,9 +13,98 @@ $("#pesquiseContato").on("keyup", function() {
     });
 });
 
-for (x=0; x<10; x++) {
+if (screen.width < 720) {
+    $('#main').hide()
+    $('#msgErroResolucao').show()
+}
+
+function mostrarChat(id) {
+    const chats = document.getElementsByClassName("chat-box");
+    for (let i = 0; i < chats.length; i++) {
+        chats[i].style.display = "none";
+    }
+
+    const chatSelecionado = document.getElementById(id);
+    chatSelecionado.style.display = "block";
+}
+
+for (x=0; x<9; x++) {
+boxMsg = `
+    <main id="boxMsgContato${x}" class="w3-display-container w3-animate-top chat-box" style="z-index:3;margin-left:400px;height:768px;display:none;">
+        <div class="w3-bar w3-top w3-blue" style="width:966!important;">
+            <div class="w3-left">
+                <span class="w3-bar-item">
+                    <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="user" style="width:40px;height:40px;">
+                    <span class="w3-large w3-margin-left w3-font-bold">Contato ${x}</span>
+                </span>
+            </div>
+
+            <div class="w3-right w3-padding" style="z-index:-4;">
+                <span class="w3-bar-item w3-button w3-round"><i class="fa-solid fa-magnifying-glass"></i></span>
+                <span class="w3-bar-item w3-button w3-round"><i class="fa-solid fa-ellipsis-vertical"></i></span>
+            </div>
+        </div>
+
+        <div class="w3-container" style="margin-top:60px;">
+            <div>
+                <div class="w3-card w3-round w3-border w3-white w3-padding w3-margin-top w3-bar">
+                    <span class="w3-left">
+                        <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                    </span>
+                    <span class="w3-left w3-bar-item">Olá bom dia tudo bem com vc?</span>
+                </div>
+
+
+                <div class="w3-card w3-round w3-border w3-light-gray w3-padding w3-margin-top w3-bar">
+                    <span class="w3-right">
+                        <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                    </span>
+                    <span class="w3-right w3-bar-item">Tudo sim, e com você?</span>
+                </div>
+
+
+                <div class="w3-card w3-round w3-border w3-white w3-padding w3-margin-top w3-bar" style="width:auto;">
+                    <span class="w3-left">
+                        <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                    </span>
+                    <span class="w3-left w3-bar-item">Eu vou bem tambem</span>
+                </div>
+
+
+                <div class="w3-card w3-round w3-border w3-light-gray w3-padding w3-margin-top w3-bar">
+                    <span class="w3-right">
+                        <img src="https://img.freepik.com/free-icon/user_318-159711.jpg?w=2000" alt="Usuario" style="width:28px;height:28px;">
+                    </span>
+                    <span class="w3-right w3-bar-item">Tudo certo :)</span>
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="w3-display-bottomright w3-bar">
+            <div class="w3-row">
+                <div class="w3-col w3-left" style="width:93px;">
+                    <div class="w3-bar">
+                        <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-face-smile"></i></span>
+                        <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-paperclip"></i></span>
+                    </div>
+                </div>
+                <div class="w3-col w3-right" style="width:93px;">
+                    <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-microphone"></i></span>
+                    <span class="w3-bar-item w3-button w3-text-blue"><i class="fa-solid fa-paper-plane"></i></span>
+                </div>
+                <div class="w3-rest">
+                    <input class="w3-input" type="text" placeholder="Mensagem">
+                </div>
+            </div>
+        </div>
+    </main>
+`
+$('#mostreBoxMsgContato').append(boxMsg)
+
     $("#chats").append(`
-    <li>
+    <li onclick="$('#boxMsgContato${x}').show();mostrarChat('boxMsgContato${x}')" style="cursor:pointer;">
         <div>
             <div class="w3-row w3-display-container">
                 <div class="w3-col m2" style="width:56px;">
@@ -34,12 +123,15 @@ for (x=0; x<10; x++) {
                             <a href="#" class="w3-bar-item w3-button">Apagar conversa</a>
                             <a href="#" class="w3-bar-item w3-button">Fixar conversa</a>
                             <a href="#" class="w3-bar-item w3-button">Marcar como não lida</a>
-                            <a href="javascript:$('demo${x}').hide()" class="w3-bar-item w3-button w3-center">Fechar</a>
+                            <a href="javascript:$('#demo${x}').hide()" class="w3-bar-item w3-button w3-center">Fechar</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </li>
-    `);
+    `)
 }
+
+
+//
